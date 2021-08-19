93004350ee00dd15d0bdd9d2edca0121c8f22d6c
Mauricio Tamayo <cesar.tamayo0204@gmail.com>
2021-08-19 14:32:19 -0500

    fix paths

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,11 @@ Route::prefix('login')->group(function () {
 Route::get('password/{password}',function ($password){
     return \Illuminate\Support\Facades\Hash::make($password);
 });
+
+Route::get('test',function (){
+    $array = array('a','b','c');
+    $indice= array_search('a',$array);
+
+    unset($array[2]);
+    return response()->json($array);
+});
