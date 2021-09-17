bc96ffd89e556efb2edce480bdb4d5ee63197d0e
RonnaldHaro7 <rsa.haro@yavirac.edu.ec>
2021-09-17 12:47:23 -0500

    scopes

--- app/Models/JobBoard/Experience.php
+++ app/Models/JobBoard/Experience.php
@@ -58,4 +58,52 @@ class Experience extends Model implements Auditable
     {
         return $this->belongsTo(Professional::class);
     }
+
+    // Scopes
+
+    public function scopeCustomOrderBy($query, $sorts)
+    {
+        if (!empty($sorts[0])) {
+            foreach ($sorts as $sort) {
+                $field = explode('-', $sort);
+                if (empty($field[0]) && in_array($field[1], $this->fillable)) {
+                    $query = $query->orderByDesc($field[1]);
+                } else if (in_array($field[0], $this->fillable)) {
+                    $query = $query->orderBy($field[0]);
+                }
+            }
+            return $query;
+        }
+    }
+
+    public function scopeCustomSelect($query, $fields)
+    {
+        if (!empty($fields)) {
+            $fields = explode(',', $fields);
+            array_unshift($fields, 'id');
+            return $query->select($fields);
+        }
+    }
+
+    public function scopeEmployer($query, $employer)
+    {
+        if ($employer) {
+            return $query->orWhere('name', 'ILIKE', "%$employer%");
+        }
+    }
+
+    public function scopePosition($query, $position)
+    {
+        if ($position) {
+            return $query->orWhere('name', 'ILIKE', "%$position%");
+        }
+    }
+
+    public function scopeReason_Leave($query, $reason_leave)
+    {
+        if ($reason_leave) {
+            return $query->orWhere('name', 'ILIKE', "%$reason_leave%");
+        }
+    }
+
 }
