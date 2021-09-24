2abbde99979a75458e575970fb30c15112de51c8
ErickDC <erickdc.lol@gmail.com>
2021-09-24 07:49:19 -0500

    first commit

--- routes/api/v1/private.php
+++ routes/api/v1/private.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\V1\Core\UserController;
 use App\Http\Controllers\V1\Core\FileController;
 use App\Http\Controllers\V1\Core\CatalogueController;
+use App\Http\Controllers\V1\LicenseWork\ProjectController;
 use App\Http\Controllers\V1\JobBoard\AcademicFormationController;
 use App\Http\Controllers\V1\JobBoard\CourseController;
 
 /***********************************************************************************************************************
  * Route license Work
  **********************************************************************************************************************/
-
 
