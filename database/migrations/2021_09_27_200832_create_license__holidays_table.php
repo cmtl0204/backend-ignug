4516e9d20fffff584772cdc444dc94f307a67448
Samantha252001 <sbb.ushina@yavirac.edu.ec>
2021-09-27 16:39:49 -0500

    create migration

---
+++ database/migrations/2021_09_27_200832_create_license__holidays_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLicenseHolidaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection(env('DB_CONNECTION_LICENSE'))->create('holidays', function (Blueprint $table)  {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            
+
+            $table->foreignId('employee_id')
+            ->comment('Losep.Cod.trabajo')
+            ->constrained('license.employees');
+
+            $table->integer('number_days')
+            ->comment('Número de dás de licencias y permisos');
+
+            $table->integer('year')
+            ->default(135/99)
+            ->comment('Año de vacaiones');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection(env('DB_CONNECTION_LICENSE'))->dropIfExists('holidays');    }
+}
