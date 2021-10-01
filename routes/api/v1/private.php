4525f41d618a1ba2a4497d6018e7650819055884
LuisGerardo17 <you@example.com>
2021-10-01 14:18:57 -0500

    create routes

--- routes/api/v1/private.php
+++ routes/api/v1/private.php
@@ -4,10 +4,51 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\V1\Core\UserController;
 use App\Http\Controllers\V1\Core\FileController;
 use App\Http\Controllers\V1\Core\CatalogueController;
+use App\Http\Controllers\V1\Core\ReasonController;
+use App\Http\Controllers\V1\Core\EmployeeController;
+
+
+/***********************************************************************************************************************
+ * Route license Work REASON
+<<<<<<< HEAD
+ **********************************************************************************************************************/
+
+Route::apiResource('reasons', ReasonController::class);
+
+Route::prefix('reason')->group(function () {
+    Route::patch('destroys', [ReasonController::class, 'destroys']);
+});
+
+Route::prefix('reason/{reason}')->group(function () {
+    Route::prefix('file')->group(function () {
+        Route::get('{file}/download', [ReasonController::class, 'indexFiles']);
+        Route::get('{file}', [ReasonController::class, 'showFile']);
+        Route::post('', [ReasonController::class, 'uploadFile']);
+        Route::put('{file}', [ReasonController::class, 'updateFile']);
+        Route::delete('{file}', [ReasonController::class, 'destroyFile']);
+        Route::patch('', [ReasonController::class, 'destroyFiles']);
+    });
+});
 
 
 
 /***********************************************************************************************************************
- * Route license Work
+ * Route license Work EMPLOYEE
 <<<<<<< HEAD
  **********************************************************************************************************************/
+Route::apiResource('employees', EmployeeController::class);
+
+Route::prefix('employee')->group(function () {
+    Route::patch('destroys', [ReasonController::class, 'destroys']);
+});
+
+Route::prefix('employee/{employee}')->group(function () {
+    Route::prefix('file')->group(function () {
+        Route::get('{file}/download', [EmployeeController::class, 'indexFiles']);
+        Route::get('{file}', [EmployeeController::class, 'showFile']);
+        Route::post('', [EmployeeController::class, 'uploadFile']);
+        Route::put('{file}', [EmployeeController::class, 'updateFile']);
+        Route::delete('{file}', [EmployeeController::class, 'destroyFile']);
+        Route::patch('', [EmployeeController::class, 'destroyFiles']);
+    });
+});
