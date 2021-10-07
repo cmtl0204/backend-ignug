8244b3301207f9b4fc3335a33bced49ea6c8a843
unknown <mjp.ortega@yavirac.edu.ec>
2021-10-06 19:00:22 -0500

    Controllers (Dependence y Dependence User)

---
+++ app/Http/Controllers/V1/LicenseWork/DependenceController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers\V1\LicenseWork;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\LicenseWork\Dependence\DestroysDependenceRequest;
+use App\Http\Requests\V1\LicenseWork\Dependence\IndexDependenceRequest;
+use App\Http\Requests\V1\LicenseWork\Dependence\StoreDependenceRequest;
+use App\Http\Requests\V1\LicenseWork\Dependence\UpdateDependenceRequest;
+use App\Http\Resources\V1\LicenseWork\DependenceCollection;
+use App\Http\Resources\V1\LicenseWork\DependenceResource;
+use App\Models\LicenseWork\Dependence;
+use Illuminate\Http\Request;
+
+class DependenceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(IndexDependenceRequest $request)
+    {
+        $sorts = explode(',', $request->sort);
+
+        $dependence = Dependence::customOrderBy($sorts)
+            ->name($request->input('name'))
+            ->paginate($request->per_page);
+
+        return (new DependenceCollection($dependence))
+            ->additional([
+                'msg' => [
+                    'summary' => 'success',
+                    'detail' => '',
+                    'code' => '200'
+                ]
+            ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreDependenceRequest $request)
+    {
+        $dependence = new Dependence();
+
+        $dependence->name = $request->input('name');
+        $dependence->save();
+
+        return (new DependenceResource($dependence))
+            ->additional([
+                'msg' => [
+                    'summary' => 'Registro Creado',
+                    'detail' => '',
+                    'code' => '200'
+                ]
+            ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Dependence $dependence)
+    {
+        return (new dependenceResource($dependence))
+            ->additional([
+                'msg' => [
+                    'summary' => 'success',
+                    'detail' => '',
+                    'code' => '200'
+                ]
+            ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateDependenceRequest $request, Dependence $dependence)
+    {
+
+        $dependence->name = $request->input('name');
+        $dependence->save();
+
+        return (new DependenceResource($dependence))
+            ->additional([
+                'msg' => [
+                    'summary' => 'Registro Actualizado',
+                    'detail' => '',
+                    'code' => '200'
+                ]
+            ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Dependence $dependence)
+    {
+        $dependence->delete();
+        return (new DependenceResource($dependence))
+            ->additional([
+                'msg' => [
+                    'summary' => 'Registro Eliminado',
+                    'detail' => '',
+                    'code' => '201'
+                ]
+            ]);
+    }
+
+    public function destroys(DestroysDependenceRequest $request)
+    {
+        $dependence = DependenceResource::whereIn('id', $request->input('ids'))->get();
+        DependenceResource::destroy($request->input('ids');
+
+        return (new DependenceCollection($dependence))
+            ->additional([
+                'msg' => [
+                    'summary' => 'Registros Eliminados',
+                    'detail' => '',
+                    'code' => '201'
+                ]
+            ]);
+    }
+}
