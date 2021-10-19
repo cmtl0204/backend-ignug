2c8cd5ee9ce37a1735d5a138fc6cb4dd33c8c56f
didyer1218 <dao.gamboa@yavirac.edu.ec>
2021-10-19 14:13:47 -0500

    Create request

---
+++ app/Http/Requests/V1/Uic/Enrollments/StoreMeshStudentRequest.php
@@ -0,0 +1,43 @@
+
+<?php
+
+namespace App\Http\Requests\V1\Custom\Example;
+
+use App\Http\Requests\V1\Custom\CustomFormRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCustomRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+            'field_example' => ['required']
+        ];
+
+        return CustomFormRequest::rules($rules);
+    }
+
+    public function attributes()
+    {
+        $attributes = [
+            'field_example' => 'campo de ejemplo',
+        ];
+
+        return CustomFormRequest::attributes($attributes);
+    }
+}
---
+++ app/Http/Requests/V1/Uic/Enrollments/UpdateExampleRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\V1\Custom\Example;
+
+use App\Http\Requests\V1\Custom\CustomFormRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCustomRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+            'field_example' => ['required'],
+        ];
+
+        return CustomFormRequest::rules($rules);
+    }
+
+    public function attributes(): array
+    {
+        $attributes = [
+            'field_example' => 'campo de ejemplo',
+        ];
+
+        return CustomFormRequest::attributes($attributes);
+    }
+}
