932ad23ae2f69af95ec6e0cc0054523555a4d6e9
bryanPEREZ1497 <bam.perez@yavirac.edu.ec>
2021-10-21 15:58:14 -0500

    Create factories files

---
+++ database/factories/Uic/EnrollmentFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Factories\Uic;
+
+use App\Models\Uic\Enrollment;
+use App\Models\Uic\Modality;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EnrollmentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Enrollment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $modalities = Modality::get();
+        $schoolPeriods = SchoolPeriod::get();
+        
+        return [
+            'modality_id' => $modalities[rand(0, sizeof($modalities) - 1)],
+            'school_period_id' => $schoolPeriods[rand(0, sizeof($schoolPeriods) - 1)],
+            'mesh_student_id' => $modalities[rand(0, sizeof($modalities) - 1)],
+            'state_id' => $modalities[rand(0, sizeof($modalities) - 1)],
+            'planning_id' => $modalities[rand(0, sizeof($modalities) - 1)],
+            'registered_at' => $this->faker->date(),
+            'code' => $this->faker->word(),
+            'observations' => $this->faker->words(3)
+        ];
+    }
+}
+
+// $professionals = Professional::get();
+// $types = Catalogue::where('type', 'COURSE_EVENT_TYPE')->get();
+// $certifications = Catalogue::where('type', 'COURSE_CERTIFICATION_TYPE')->get();
+// // $certifications = Catalogue::all()->only([58, 59]);
+// $areas = Catalogue::where('type', 'COURSE_AREA')->get();
+
+// return [
+//     'code' => $this->faker->word(),
+//     'professional_id' => $professionals[rand(0, sizeof($professionals) - 1)],
+//     'type_id' => $types[rand(0, sizeof($types) - 1)],
+//     'certification_type_id' => $certifications[rand(0, sizeof($certifications) - 1)],
+//     'area_id' => $areas[rand(0, sizeof($areas) - 1)],
+//     'name' => $this->faker->word(),
+//     'description' => $this->faker->text(30),
+//     'started_at' => $this->faker->date(),
+//     'ended_at' => $this->faker->date(),
+//     'hours' => $this->faker->randomDigit(),
+//     'institution' => $this->faker->word(),
+// ];
---
+++ database/seeders/UicSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->createCustomSeeder();
+    }
+
+    private function createCustomSeeder()
+    {
+
+    }
+}
